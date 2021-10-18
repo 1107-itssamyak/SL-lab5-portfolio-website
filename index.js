340fe45afeaa6941540fa855b6e1b386eb24cea8
1107-itssamyak <samyak.mehta11@gmail.com>
2021-10-18 10:11:06 +0530

    initial and final commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let title = document.getElementById("title");
+let textbox = document.getElementById("textbox");
+
+function home() {
+    title.innerHTML = "Home";
+    let ans = `This is Home description`;
+    textbox.innerHTML = ans;
+}
+function aboutme() {
+    title.innerHTML = "About Me";
+    let ans = `This is About me description`;
+    textbox.innerHTML = ans;
+}
+function education() {
+    title.innerHTML = "Education";
+    let ans = `This is Education description`;
+    textbox.innerHTML = ans;
+}
+function resume() {
+    title.innerHTML = "Resume";
+    let ans = `This is Resume description`;
+    textbox.innerHTML = ans;
+}
+function hobbies() {
+    title.innerHTML = "Hobbies";
+    let ans = `This is Hobbies description`;
+    textbox.innerHTML = ans;
+}
+function contactme() {
+    title.innerHTML = "Contact Me";
+    let ans = `This is Contact Me description`;
+    textbox.innerHTML = ans;
+}
+
